453c7fbd0763f8bdfe035b1ad35d704959245292
Pavel Pavlenko <61290607+K1uV@users.noreply.github.com>
2020-06-17 19:52:24 +0300

    Add files via upload

---
+++ lection18_ex_1.py
@@ -0,0 +1,6 @@
+def gamb():
+    name = input('Input your name: ')
+    age = input('Input your age: ')
+    print(f'Hello {name}, your age is {age}'.format(name, age))
+
+gamb()
---
+++ lection_18_ex_2.py
@@ -0,0 +1,4 @@
+from lection18_ex_1 import gamb
+print('Welcome to our shop')
+
+gamb()
